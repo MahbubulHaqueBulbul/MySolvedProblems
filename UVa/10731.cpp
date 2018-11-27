e3f773f22c28bb0257b2282210f72787e4e32e3e
MahbubulHaqueBulbul <34815066+MahbubulHaqueBulbul@users.noreply.github.com>
2018-11-28 03:30:22 +0600

    Update 10731.cpp

--- UVa/10731.cpp
+++ UVa/10731.cpp
@@ -32,7 +32,7 @@ public:
 int Set(int N,int pos) { return N=N|(1<<pos); }
 int reset(int N,int pos){ return N=N&~(1<<pos);}
 bool check(int N,int pos){  return (bool) (N&(1<<pos));}
- 
+ ffggg
 vector< vi > AdjLst,SCC;
 vi dfs_num,dfs_low,Stack;
 vector<bool> visited;
